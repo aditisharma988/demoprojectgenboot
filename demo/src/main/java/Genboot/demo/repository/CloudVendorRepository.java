0a94291a2fd82fcd8ec81588cd7bb4afe7abdd4d
aditisharma988 <aditisharma140802@gmail.com>
2024-09-23 14:19:07 +0530

    Creates repository file extending jpa repository for CRUD operations.

---
+++ demo/src/main/java/Genboot/demo/repository/CloudVendorRepository.java
@@ -0,0 +1,8 @@
+package Genboot.demo.repository;
+
+import Genboot.demo.model.CloudVendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CloudVendorRepository extends JpaRepository<CloudVendor, Long> {
+
+}
