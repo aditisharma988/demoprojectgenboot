d56074f290e4c94c8dee9e44dd5a58778cdaf478
aditisharma988 <aditisharma140802@gmail.com>
2024-09-23 14:21:38 +0530

    Creates controller file having each http methods logic maps the request and calls corresponding service method.

---
+++ demo/src/main/java/Genboot/demo/controller/CloudVendorApiServiceController.java
@@ -0,0 +1,66 @@
+package Genboot.demo.controller;
+import Genboot.demo.model.CloudVendor;
+import Genboot.demo.services.CloudVendorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorApiServiceController {
+
+    @Autowired
+    private CloudVendorService cloudVendorService;
+
+   //get specific cloud vendor
+    @GetMapping("/{Id}")
+    public CloudVendor getCloudVendorDetails(@PathVariable("Id") Long Id )
+    {
+        return cloudVendorService.get(Id);
+    }
+
+    //get all cloud vendors
+    @GetMapping()
+    public List<CloudVendor> getAllCloudVendorDetails()
+    {
+        return cloudVendorService.getAll();
+    }
+
+    //create a cloud vendor
+    @PostMapping("/create")
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        cloudVendorService.create(cloudVendor);
+        return " Cloud vendor created successfully";
+    }
+
+
+    //update a cloud vendor
+    @PutMapping("{Id}/update")
+    public String updateCloudVendorDetails(@PathVariable("Id") Long Id,@RequestBody CloudVendor cloudVendor )
+    {
+        cloudVendorService.update(Id, cloudVendor);
+        return "A Cloud vendor updated successfully";
+    }
+
+
+    //update all cloud vendors
+    @PutMapping("/update")
+    public String updateAllCloudVendorDetails(@RequestBody List<CloudVendor> cloudVendors )
+    {
+        cloudVendorService.updateAll(cloudVendors);
+        return "All Cloud vendors updated successfully";
+    }
+
+
+    //delete a cloud vendor
+    @DeleteMapping("/{Id}/delete")
+    public String deleteCloudVendorDetails(@PathVariable("Id") Long Id )
+    {
+         cloudVendorService.delete(Id);
+         return "Cloud vendor deleted successfully";
+
+    }
+
+}
