608a9af2b9871e61f7ada40bacb2849998662e55
aditisharma988 <aditisharma140802@gmail.com>
2024-09-24 18:48:48 +0530

    handles exception using particular entity and imports package for same.

--- demo/src/main/java/Genboot/demo/repository/CloudVendorRepository.java
+++ demo/src/main/java/Genboot/demo/repository/CloudVendorRepository.java
@@ -3,6 +3,10 @@ package Genboot.demo.repository;
 import Genboot.demo.model.CloudVendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CloudVendorRepository extends JpaRepository<CloudVendor, Long> {
+   Optional<CloudVendor> findByVendorPhoneNumber(String vendorPhoneNumber);
 
 }
